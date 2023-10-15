e5c25d7eface13e08fc9a52f1430587ef494295f
yyf999999999 <122949113+yyf999999999@users.noreply.github.com>
2023-10-15 22:27:00 +0900

    first commit

---
+++ テスト.js
@@ -0,0 +1,4 @@
+textWarehouse=[ {jn:"国語",en:"kokugo"},{jn:"社会",en:"syakai"},{jn:"微分積分",en:"bibunnsekibunn"}];
+console.log(textWarehouse);
+JSON.stringify(textWarehouse);
+console.log(textWarehouse);
